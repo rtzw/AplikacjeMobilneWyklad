8f85ced3b4073037c6b79bce9b3a733ba527df18
rtzw <72400704+rtzw@users.noreply.github.com>
2022-08-17 15:19:59 +0200

    New fragments

--- app/src/main/java/com/example/am_wyklad/Fragments/AddChallanges.kt
+++ app/src/main/java/com/example/am_wyklad/Fragments/AddChallanges.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.example.am_wyklad.R
 
 
 class AddChallanges : Fragment() {
+    lateinit var backButton: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,15 @@ class AddChallanges : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_challanges, container, false)
+        backButton = view.findViewById(R.id.backButton)
+
+        backButton.setOnClickListener(){
+            val newProfile = NewProfile();
+            val fragmentTransaction: FragmentTransaction =
+                fragmentManager!!.beginTransaction()
+            fragmentTransaction.replace(R.id.mainActivity, newProfile)
+            fragmentTransaction.commit()
+        }
         return view
     }
 
--- app/src/main/java/com/example/am_wyklad/Fragments/AddParticipants.kt
+++ app/src/main/java/com/example/am_wyklad/Fragments/AddParticipants.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.example.am_wyklad.R
 
 class AddParticipants : Fragment() {
+    lateinit var backButton: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,15 @@ class AddParticipants : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_participants, container, false)
+        backButton = view.findViewById(R.id.backButton)
+
+        backButton.setOnClickListener(){
+            val newProfile = NewProfile();
+            val fragmentTransaction: FragmentTransaction =
+                fragmentManager!!.beginTransaction()
+            fragmentTransaction.replace(R.id.mainActivity, newProfile)
+            fragmentTransaction.commit()
+        }
         return view
     }
 

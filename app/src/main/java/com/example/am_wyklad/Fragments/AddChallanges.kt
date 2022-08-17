9c1f5cfe3e4212e24e541433ffe36a15e13c5ba8
rtzw <72400704+rtzw@users.noreply.github.com>
2022-08-17 14:55:07 +0200

    Big changes

---
+++ app/src/main/java/com/example/am_wyklad/Fragments/AddChallanges.kt
@@ -0,0 +1,27 @@
+package com.example.am_wyklad.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.am_wyklad.R
+
+
+class AddChallanges : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_challanges, container, false)
+        return view
+    }
+
+
+}

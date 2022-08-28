9ca4acfb52187e1840a7a986ba89e818737a5252
rtzw <72400704+rtzw@users.noreply.github.com>
2022-08-28 15:11:57 +0200

    v1.2

---
+++ app/src/main/java/com/example/am_wyklad/Adapters/ChallengesRecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.am_wyklad.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.am_wyklad.R
+
+class ChallengesRecyclerAdapter(private val dataSet: MutableList<String>) : RecyclerView.Adapter<ChallengesRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.challenges_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+
+    override fun getItemCount() = dataSet.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var challenge: TextView
+        var select: CheckBox
+
+        init {
+            challenge = itemView.findViewById(R.id.challenge)
+            select = itemView.findViewById(R.id.checkbox)
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.challenge.text = dataSet[position]
+
+        holder.select.setOnClickListener(){
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/am_wyklad/Fragments/AddChallenges.kt
+++ app/src/main/java/com/example/am_wyklad/Fragments/AddChallenges.kt
@@ -1,16 +1,28 @@
 package com.example.am_wyklad.Fragments
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.am_wyklad.Adapters.ChallengesRecyclerAdapter
+import com.example.am_wyklad.Adapters.ParticipantsRecyclerAdapter
 import com.example.am_wyklad.R
+import com.example.am_wyklad.StaticVariables
 
 
 class AddChallenges : Fragment() {
     lateinit var backButton: View
+    lateinit var addCustom: Button
+    private lateinit var challengesRecyclerAdapter: ChallengesRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +35,34 @@ class AddChallenges : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_challenges, container, false)
         backButton = view.findViewById(R.id.backButton)
+        addCustom = view.findViewById(R.id.addCustomChallenge)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycleViewChallenges)
+        challengesRecyclerAdapter = ChallengesRecyclerAdapter(StaticVariables.addedChallenges)
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = challengesRecyclerAdapter
+
+        addCustom.setOnClickListener(){
+            val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(activity)
+            builder.setTitle("Challenge")
+            val input = EditText(activity)
+            input.setHint("Input challenge")
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            builder.setView(input)
+
+            builder.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                var m_Text = input.text.toString()
+                StaticVariables.addedParticipants.add(m_Text)
+                challengesRecyclerAdapter = ChallengesRecyclerAdapter(StaticVariables.addedChallenges)
+                recyclerView.adapter = challengesRecyclerAdapter
+                challengesRecyclerAdapter.notifyDataSetChanged()
+
+            })
+            builder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+            builder.show()
+
+        }
 
         backButton.setOnClickListener(){
             val newProfile = NewProfile();
